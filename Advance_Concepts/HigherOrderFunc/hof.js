ad2a14715ebea6db23f1456fe3d1dd1a69d4d658
Demojsdev <145244812+Demojsdev@users.noreply.github.com>
2023-09-17 15:27:53 +0000

    higher order function example write

--- Advance_Concepts/HigherOrderFunc/hof.js
+++ Advance_Concepts/HigherOrderFunc/hof.js
@@ -1,1 +1,37 @@
-console.log('hello world')
+const radius = [1,2,3,4,6,7]
+
+function area(radius) {
+ let a = []
+  for(let i  = 0; i< radius.length; i++){
+   a.push( Math.PI * radius[i] * radius[i])
+  }
+  return a
+}
+
+function circumference(radius) {
+ let a = []
+  for(let i  = 0; i< radius.length; i++){
+    a = [...a, 2 * Math.PI * radius[i]]
+  }
+  return a
+}
+function diameter(radius) {
+ let a = []
+  for(let i  = 0; i< radius.length; i++){
+    a = [...a, Math.PI * radius[i] * radius[i]]
+  }
+  return a
+}
+
+function filterFunction(arr, callback) {
+  const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    filteredArr.push(arr[i])
+    callback(arr[i]) ? arr[i] : null;
+  }
+  return filteredArr;
+}
+
+console.log(filterFunction(radius, (data) =>{
+  return  data % 2 != 0
+}))
